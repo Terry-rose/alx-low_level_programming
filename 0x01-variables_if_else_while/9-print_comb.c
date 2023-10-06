b74270823640d0483505fc60b5056c9c17e365ea
Terry-rose <2216mayet@gmail.com>
2023-10-06 14:46:54 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,21 @@
 
 
 int main() {
-    int i, j, k, l;
+    int i, j, k;
 
-    for (i = 0; i <= 9; ++i) {
-        for (j = i + 1; j <= 9; ++j) {
-            for (k = j + 1; k <= 9; ++k) {
-                for (l = k + 1; l <= 9; ++l) {
-                    /*Print numbers separated by ", "*/
-                    putchar(i + '0');
+    for (i = 0; i < 8; ++i) {
+        for (j = i + 1; j < 9; ++j) {
+            for (k = j + 1; k < 10; ++k) {
+                /*Print numbers separated by ", "*/
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+
+                /*Print separator and newline character after each combination*/
+                if (i != 7 || j != 8 || k != 9) {
                     putchar(',');
                     putchar(' ');
-                    putchar(j + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(k + '0');
-                    putchar(',');
-                    putchar(' ');
-                    putchar(l + '0');
+                } else {
                     putchar('\n');
                 }
             }
