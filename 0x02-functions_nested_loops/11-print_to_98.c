2315e45566baeec35435051bb3fc5c1f1e3abb2b
Terry-rose <2216mayet@gmail.com>
2023-10-11 14:41:28 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,29 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
+    int i;
+
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
             printf("%d", i);
-            if (i != 98) {
+            if (i != 98)
+            {
                 printf(", ");
-}
-}
-} else {
-	for (int i = n; i >= 98; i--) {
-		printf("%d", i);
-		if (i != 98) {
-			printf(", ");
-}
-}
-}
-printf("\n");
+            }
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            printf("%d", i);
+            if (i != 98)
+            {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
 }
