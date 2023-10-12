04bdc65e829abe96db88a971f807bc32beb3c414
Terry-rose <2216mayet@gmail.com>
2023-10-12 12:39:27 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,16 @@
 
 int _putchar(char c);
 void print_to_98(int n);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void)
+int add(int a, int b);
+int _islower(int c)
+int main(void)
+int print_sign(int n)
+void times_table(void)
+int _abs(int n);
 
 #endif /* MAIN_H */
