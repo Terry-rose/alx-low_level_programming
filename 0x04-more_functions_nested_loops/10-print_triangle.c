4a55156fef67998d7f75336481d732570c3977aa
Terry-rose <2216mayet@gmail.com>
2023-10-13 13:37:29 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of a given size using '#'
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+    if (size <= 0) {
+        _putchar('\n');
+    } else {
+        int i, j;
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size - i - 1; j++) {
+                _putchar(' ');
+            }
+            for (j = 0; j < i + 1; j++) {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,4 +27,7 @@ void print_diagonal(int n);
 int _putchar(char c);
 void print_square(int size);
 
+int _putchar(char c);
+void print_triangle(int size);
+
 #endif /* MAIN_H */
