7d801934cb75a5c4152337cb50ba32437165a3e3
Terry-rose <2216mayet@gmail.com>
2023-10-23 12:44:39 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+char *_memset(char *s, char b, unsigned int n) {
+    char *ptr = s;
+    while (n > 0) {
+        *ptr = b;
+        ptr++;
+        n--;
+    }
+    return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+
+#define MAX_SIZE 100
+#endif /* MAIN_H */
