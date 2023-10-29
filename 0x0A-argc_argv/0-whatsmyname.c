7ecb60a027f3f01d899d2f8c1a2994bf886e3065
Terry-rose <2216mayet@gmail.com>
2023-10-29 14:17:32 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf(%d\n", argc);
-    printf(%s\n", argv[0]);
+    printf("%d\n", argc);
+    printf("%s\n", argv[0]);
     return 0;
 }
