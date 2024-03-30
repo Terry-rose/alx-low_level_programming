7e688a3048fa32ea30636feef941280807317f9c
Terry_rose <2216mayet@gmail.com>
2024-03-29 19:27:26 -0700

    commit

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Forward declaration of a function defined in school.c
+void print_school_message();
+
+int main() {
+    // Call a function defined in school.c
+    print_school_message();
+
+    return 0;
+}
+
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Function definition for printing a school message
+void print_school_message() {
+    printf("Welcome to the school!\n");
+}
+
